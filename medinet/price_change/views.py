a4bea72fd165cbab410c4a324262326a30a43757
CyberPunk58 <pavelrip@mail.ru>
2022-12-14 16:09:33 +0300

    add price.pdf

--- medinet/price_change/views.py
+++ medinet/price_change/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Price_change
 
 def price_change(request):
-    price_change_info = Price_change.objects.all()
+    price_change_info = Price_change.objects.order_by('-date')[:10]
     return render(request, 'price_change/price_change.html', {'price_change':price_change_info})
