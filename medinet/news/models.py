ac0cc62b436e893a88ea10d64c3ff4bfc64c8b09
CyberPunk58 <pavelrip@mail.ru>
2022-12-14 09:10:56 +0300

    start project

---
+++ medinet/news/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class News(models.Model): # Модель для новостей на сайте
+    title = models.CharField(max_length=200)  #Заголовок новости
+    description = models.CharField(max_length=300) #Описание новости
+    image = models.ImageField(upload_to='news/images/')
+    url = models.URLField(blank=True) #Для ссылок Blank - открывает ссылкив новом окне
---
+++ medinet/price_change/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PriceChangeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'price_change'
