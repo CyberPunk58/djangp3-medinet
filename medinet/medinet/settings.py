5cedcbe154e58ab903c29d91714e200fa68d4383
CyberPunk58 <pavelrip@mail.ru>
2022-12-20 17:20:56 +0300

    block added

--- medinet/medinet/settings.py
+++ medinet/medinet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p&i-!#q0+#idk$k_*&@8y7-0()#8j)*#%x#zz#nu08(=#nusi(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.123', 'localhost']
 
 
 # Application definition
